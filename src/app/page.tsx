5aae5573f6a308bbb0e94c9b1d43cd2be64843de
gustavormuller <gusrmuller@gmail.com>
2024-10-03 03:49:16 -0300

    feat: logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="">
+      <Image
+        src="/logo-rectangle.png"
+        alt="Logo da Santiago Vinhos"
+        width={150}
+        height={75}
+      />
       <h1>Santiago Vinhos</h1>
     </main>
   );
