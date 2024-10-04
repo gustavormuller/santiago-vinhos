6314eb426de78d2b1a8de159664d1ba75bb160a5
gustavormuller <gusrmuller@gmail.com>
2024-10-04 20:38:01 -0300

    feat: dropdown menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
-import Image from "next/image"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Menu } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <Image
-        src="/logo-rectangle.png"
-        alt="Logo da Santiago Vinhos"
-        width={150}
-        height={75}
-      />
-      <h1>Santiago Vinhos</h1>
+      <nav className="flex px-5 items-center justify-between bg-[#bb2343]">
+        <Image
+          src="/logo-square.png"
+          alt="Logo da Santiago Vinhos"
+          width={90}
+          height={90}
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Menu color="#fff" size={32} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
     </main>
   );
 }
