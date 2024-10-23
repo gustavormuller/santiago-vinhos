144f1c82458a6b498733fdf4c95c0fd2aabdbaea
gustavormuller <gusrmuller@gmail.com>
2024-10-23 15:37:16 -0300

    feat: new logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import ToggleMenu from "@/components/ui/ToggleMenu";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
@@ -6,38 +7,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="">
-      <nav className="flex px-5 lg:px-12 py-[5px] items-center justify-between bg-[#bb2343]">
-        <Image
-          src="/logo-cut.png"
-          alt="Logo da Santiago Vinhos"
-          width={90}
-          height={70}
-          className="lg:w-[100px]"
-        />
-        <div className="flex items-center lg:hidden">
-          <ToggleMenu />
-        </div>
-        <div className="hidden lg:flex items-center text-[#F9DEE5] text-lg font-poppins">
-          <a href="#" className="mr-10 relative group">
-            <span>Início</span>
-            <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#F9DEE5] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-          </a>
-          <a href="#" className="mr-10 relative group">
-            <span>Sobre</span>
-            <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#F9DEE5] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-          </a>
-          <a href="#" className="mr-10 relative group">
-            <span>Rótulos</span>
-            <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#F9DEE5] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-          </a>
-          <a href="#" className="relative group">
-            <span>Contato</span>
-            <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#F9DEE5] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-          </a>
-        </div>
-      </nav>
-      {/* <Image src="/wine-pouring.png" width={300} height={300} alt="Garrafa de vinho" className="ml-auto mt-32" /> */}
-      <section className="flex flex-col bg-[#f7eaee] py-10 font-libre">
+      <Navbar />
+      <section className="flex flex-col bg-[#f7eaee] pb-10 pt-24 font-libre">
         <div className="flex flex-row">
           <div className="px-3 pb-5 relative z-10">
             <h1 className="text-[50px] leading-[48px] font-medium text-[#bb2343] w-[230px] font-eb">
