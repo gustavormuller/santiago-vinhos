ee5dd49e0d23f627e383ac34b25745dc187e021c
gustavormuller <gusrmuller@gmail.com>
2024-10-23 15:37:32 -0300

    feat: animated navbar when scroll

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useState, useEffect } from "react";
+import ToggleMenu from "@/components/ui/ToggleMenu";
+import Image from "next/image";
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <nav
+      className={`flex px-5 lg:px-12 py-[3px] items-center justify-between bg-[#f7eaee] fixed w-full z-50 transition-transform duration-300 ${
+        showNavbar ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <Image
+        src="/logo-wine-bg-removed.png"
+        alt="Logo da Santiago Vinhos"
+        width={85}
+        height={70}
+        className="lg:w-[100px]"
+      />
+      <div className="flex items-center lg:hidden">
+        <ToggleMenu />
+      </div>
+      <div className="hidden lg:flex items-center text-[#bb2343] text-2xl font-medium font-eb">
+        <a href="#" className="mr-16 relative group">
+          <span>Início</span>
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#bb2343] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+        </a>
+        <a href="#" className="mr-16 relative group">
+          <span>História</span>
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#bb2343] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+        </a>
+        <a href="#" className="mr-16 relative group">
+          <span>Rótulos</span>
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#bb2343] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+        </a>
+        <a href="#" className="relative group">
+          <span>Contato</span>
+          <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#bb2343] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/ui/ToggleMenu.tsx
+++ src/components/ui/ToggleMenu.tsx
@@ -20,28 +20,28 @@ const ToggleMenu = () => {
           className={`absolute top-0 left-0 transition-all duration-300 ${
             isOpen ? "opacity-0 rotate-90" : "opacity-100 rotate-0"
           }`}
-          color="#F9DEE5"
+          color="#bb2343"
           size={32}
         />
         <X
           className={`absolute top-0 left-0 transition-all duration-300 ${
             isOpen ? "opacity-100 rotate-0" : "opacity-0 rotate-90"
           }`}
-          color="#F9DEE5"
+          color="#bb2343"
           size={32}
         />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-[#bb2343] text-[#F9DEE5] pr-4 pl-2 pt-2 pb-3 border-[#F9DEE5] border-2">
-        <DropdownMenuItem className="text-base font-poppins">
+      <DropdownMenuContent className="bg-[#f7eaee] text-[#bb2343] pr-4 pl-2 pt-2 pb-3 border-[#ebcad1] border-2 mr-1">
+        <DropdownMenuItem className="text-[22px] font-eb">
           Início
         </DropdownMenuItem>
-        <DropdownMenuItem className="text-base font-poppins">
-          Sobre
+        <DropdownMenuItem className="text-[22px] font-eb">
+          História
         </DropdownMenuItem>
-        <DropdownMenuItem className="text-base font-poppins">
+        <DropdownMenuItem className="text-[22px] font-eb">
           Rótulos
         </DropdownMenuItem>
-        <DropdownMenuItem className="text-base font-poppins">
+        <DropdownMenuItem className="text-[22px] font-eb">
           Contato
         </DropdownMenuItem>
       </DropdownMenuContent>
